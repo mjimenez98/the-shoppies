3ce21a1d106729de03b187ed33f38cb5f368a00e
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-08-31 00:33:50 -0400

    Add test to verify title "The Shoppies" is in navbar

--- src/containers/App.test.tsx
+++ src/containers/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import Header from '../components/Header';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders title in navbar', () => {
+  const { getByText } = render(<Header />);
+  const title = getByText(/The Shoppies/);
+  expect(title).toBeInTheDocument();
 });
