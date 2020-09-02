bb88d422e6b7482e9a43dcbae28d4117f5cfa0de
Miguel Jimenez <migueleduardo298@hotmail.com>
2020-09-02 14:00:06 -0400

    Call OMDb API and display results for a search

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,20 +3,66 @@ import {
   Col, Container, Form, Row,
 } from 'bootstrap-4-react';
 
+class Movie {
+  title: string;
+  year: string;
+
+  constructor(title: string, year: string) {
+    this.title = title;
+    this.year = year;
+  }
+}
+
 interface States {
+  movieResults: Array<Movie>,
   searchValue: string
 }
 
 class Search extends React.Component<{}, States> {
   constructor(props: any) {
     super(props);
-    this.state = { searchValue: '' };
+    this.state = {
+      movieResults: [],
+      searchValue: ''
+    };
 
+    this.getMoviesFromOMDb = this.getMoviesFromOMDb.bind(this);
     this.handleChange = this.handleChange.bind(this);
+  }
+
+  private async getMoviesFromOMDb(search: string) {
+    try {
+      const apiKey = '';
+      const type = 'movie';
+      const s = search;
+
+      await fetch('http://www.omdbapi.com/?' +
+        'apikey=' + apiKey +
+        '&type=' + type +
+        '&s=' + s)
+      .then(response => response.json())
+      .then(data => this.updateMovies(data));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  private async updateMovies(res: any) {
+    if(res['Response'] === 'True') {
+      const resMovies = res['Search'];
+      const movies = resMovies.map((x: { [x: string]: string; }) => {
+        return new Movie(x['Title'], x['Year']);
+      });
+
+      this.setState({ movieResults: movies})
+    } else {
+      this.setState({ movieResults: [] })
+    }
   }
 
   private handleChange(event: any) {
     this.setState({ searchValue: event.target.value });
+    this.getMoviesFromOMDb(event.target.value);
   }
 
   render() {
@@ -36,6 +82,17 @@ class Search extends React.Component<{}, States> {
                 />
               </Form.Group>
             </Form>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+          <div>
+            {this.state.movieResults.map((movie, index) => (
+              <li key={index}>
+                {movie.title + ' - ' + movie.year}
+              </li>
+            ))}
+          </div>
           </Col>
         </Row>
       </Container>
